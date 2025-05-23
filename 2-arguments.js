81136ca8271c3ffded42360c6ac054e05143ddee
yazie mulie <yaziemulie17@gmail.com>
2025-05-23 10:59:11 -0200

    add argument

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,4 @@
-const processargv = 'ya';
+const processargv = 'yazie';
 if (processargv.length <= 2) {
     console.log("No argument");
     } else if (processargv.length === 3) {
@@ -6,19 +6,4 @@ if (processargv.length <= 2) {
     } else {
     console.log("Arguments found");
     }
-    const processargv1 = 'yaz';
-if (processargv.length <= 2) {
-    console.log("No argument");
-    } else if (processargv1.length === 3) {
-    console.log("Argument found");
-    } else {
-    console.log("Arguments found");
-    }
-    const processargv2 = 'yazie';
-if (processargv.length <= 2) {
-    console.log("No argument");
-    } else if (processargv2.length === 3) {
-    console.log("Argument found");
-    } else {
-    console.log("Arguments found");
-    }
+   
