ec22fccde9c1df7f54dd5bd912ec44c667413f10
yazie mulie <yaziemulie17@gmail.com>
2025-05-23 10:55:38 -0200

    add argument in if statement

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,23 @@
-const processargv = ['ya', 'yaz', 'y'];
+const processargv = 'ya';
 if (processargv.length <= 2) {
     console.log("No argument");
     } else if (processargv.length === 3) {
+    console.log("Argument found");
+    } else {
+    console.log("Arguments found");
+    }
+    const processargv1 = 'yaz';
+if (processargv.length <= 2) {
+    console.log("No argument");
+    } else if (processargv1.length === 3) {
+    console.log("Argument found");
+    } else {
+    console.log("Arguments found");
+    }
+    const processargv2 = 'yazie';
+if (processargv.length <= 2) {
+    console.log("No argument");
+    } else if (processargv2.length === 3) {
     console.log("Argument found");
     } else {
     console.log("Arguments found");
