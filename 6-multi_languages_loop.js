23e311d453fd08735274b46f0b6a5a15576fceeb
yazie mulie <yaziemulie17@gmail.com>
2025-05-24 08:08:03 -0200

    add for loop for display strings

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+const arr = ["C is fun", "Python is coo", "JavaScript is amazing"];
+// for(let item in arr){
+//     console.log(arr[item]);
+// }
+// for(let item of arr){
+//     console.log(item);
+// }
+for(let i =0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
