59e501044dd198377384d2c44db2f2b17d1c7fbf
yazie mulie <yaziemulie17@gmail.com>
2025-05-24 09:33:54 -0200

    add for loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,15 @@
-const arr = ["C is fun", "Python is coo", "JavaScript is amazing"];
+/*const arr = ["C is fun", "Python is coo", "JavaScript is amazing"];
 for(let item of arr) {
     console.log(item);
+}*/
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i];
+  if (i < lines.length - 1) {
+    output += "\n";
+  }
 }
+
+console.log(output);
