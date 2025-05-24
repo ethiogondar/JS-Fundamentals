cd0f7c933e9eb09791db5d628c1af9bb4d9f3eee
yazie mulie <yaziemulie17@gmail.com>
2025-05-24 10:35:24 -0200

    add factorial function

--- 10-factorial.js
+++ 10-factorial.js
@@ -2,9 +2,9 @@ const { argv } = require("node:process");
 const args = argv.slice(2)
 const a = parseInt(args[0]);
 
-const factorial = (num) => {
+function factorial (num) {
     if(Number.isNaN(num)) return 1;
-    
+
     if (num == 1) return 1
     return num * factorial(num -1);
 }
