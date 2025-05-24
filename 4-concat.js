f3d9280df77d244abe5aefcc05bbd5ef2874c74a
yazie mulie <yaziemulie17@gmail.com>
2025-05-24 07:10:13 -0200

    Add concat.js file

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,4 @@
-const args = process.argv.slice(2);
-if (args[0] !== undefined && args[1] !== undefined) {
-    console.log(${args[0]} ${String.fromCharCode(105) + String.fromCharCode(115)} ${args[1]});
-} else {
-    console.log("Insufficient arguments");
-}
+const {argv} = require("node:process");
+const args = argv.slice(2);
+
+console.log(`${args[0]} is ${args[1]}`)
